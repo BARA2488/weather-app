924e2333f98cbf9349ec86b575392e74f77da0e2
BARA2488 <mglomozda86@gmail.com>
2025-05-26 19:07:26 +0300

    WIND UPDATE

--- src/main/java/com/example/WeatherData.java
+++ src/main/java/com/example/WeatherData.java
@@ -4,6 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 public class WeatherData{
 private Main main;
+private Wind wind;
 private String name;
 private Weather[] weather;
 
@@ -32,16 +33,34 @@ public String getPressure(){
     return String.format("%.2f", mmHg);
     }
 }
+public static class Wind{
+    
+    @SerializedName("speed")
+    private double speed;
+
+    @SerializedName("deg")
+    private int direction;
+
+    @SerializedName("gust")
+    private double gust;
+
+    public double getSpeed(){ return speed; }
+    public int getDirection(){ return direction; }
+    public Double getGust(){ return gust;}
+
+    
+}
 
 public static class Weather {
     private String description;
     private String main;
-
+    
     public String getDescription(){ return description; }
     public String getMain(){ return main; }
 }
 
-public Main getMain(){ return main; }
-public String getName(){ return name; }
-public Weather[] getWeather(){return weather;}
+    public Main getMain(){ return main; }
+    public Wind getWind(){ return wind; }
+    public String getName(){ return name; }
+    public Weather[] getWeather(){return weather;}
 }
